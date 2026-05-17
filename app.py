538eff7c7a0df77c35f8ee3902cfe6ba86704516
Ray1-bashaje <42996083+Ray1-bashaje@users.noreply.github.com>
2026-05-17 17:44:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ def uliza():
         db.session.commit()
         return redirect(url_for('index'))
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
