5d11209d7b02b6bf0c2aa1e24cd09bf2c694af52
Ray1-bashaje <42996083+Ray1-bashaje@users.noreply.github.com>
2026-05-17 17:33:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ def uliza():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all() # Inatengeneza database kiotomatiki ikiwa haipo
+        db.create_all()
     app.run(debug=True)
